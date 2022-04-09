56f6f34b3ebb3db7dc767c0e34c79b1ed92b817b
Tanay Shandilya <tanay.shandilya99@gmail.com>
2022-04-10 00:33:25 +0530

    timer added

--- src/Components/TextBox.jsx
+++ src/Components/TextBox.jsx
@@ -1,5 +1,6 @@
 import styles from '../Styles/TextBox.module.css'
 import { FaLinkedin, FaEnvelope, FaInstagram } from 'react-icons/fa'
+import Timer from './Timer'
 
 const social = [
   {
@@ -28,6 +29,7 @@ export default function TextBox(){
       <div className={styles.text}>Hi, I am <br/><span className={styles.primary}>Tanay Shandilya</span></div>
       <div className={styles.desc}>I am a cyber security consultant, cloud architect, hobby developer, data science enthusiast, and also a blockchain geek. I am currently working as a Cyber Risk Consultant @ Deloitte.</div>
       <div className={styles.foot}>Website is coming soon</div>
+      <Timer/>
       <div className={styles.socialContainer}>
         {
           social.map(item => <a key={item.id} target="_blank" rel="noreferrer" href={item.url} className={styles.button} style={{color: item.color}}>
---
+++ src/Components/Timer.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import styles from '../Styles/Timer.module.css'
+
+const countDownDate = new Date("Dec 1, 2022 00:00:00").getTime()
+
+export default function Timer(){
+
+  const [Days, setDays] = React.useState(0)
+  const [Hrs, setHrs] = React.useState(0)
+  const [Mins, setMins] = React.useState(0)
+  const [Sec, setSec] = React.useState(0)
+
+  var x = setInterval(function() {
+    var now = new Date().getTime()
+    var distance = countDownDate - now
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24))
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000)
+    
+    setDays(days)
+    setHrs(hours)
+    setMins(minutes)
+    setSec(seconds)
+  
+    if (distance < 0) {
+      clearInterval(x)
+    }
+  }, 1000)
+
+  return(
+    <div className={styles.container}>
+      <div style={{marginRight: 6}}>{Days.toString().padStart(3, '0')} <span className={styles.name}>{Days <= 1 ? 'Day': 'Days'}</span></div>
+      <div style={{marginRight: 6}}>{Hrs.toString().padStart(2,'0')} <span className={styles.name}>{Hrs <= 1 ? 'Hour': 'Hours'}</span></div>
+      <div style={{marginRight: 6}}>{Mins.toString().padStart(2,'0')} <span className={styles.name}>{Mins <= 1 ? 'Min': 'Mins'}</span></div>
+      <div>{Sec.toString().padStart(2,'0')} <span className={styles.name}>{Sec <= 1 ? 'Sec': 'Secs'}</span></div>
+    </div>
+  )
+}
